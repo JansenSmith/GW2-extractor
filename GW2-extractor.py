ccf255a9b28d0171f363917c5ed9210270f4027d
Jansen Smith <jansen@bstarengineering.com>
2024-09-30 11:27:51 -0400

    fixed return issue

--- GW2-extractor.py
+++ GW2-extractor.py
@@ -5,7 +5,7 @@ with open('api_keys.txt', 'r') as opened_api_file:
     api_keys = [line.strip() for line in opened_api_file.readlines()]
     
 print(api_keys)
-return
+sys.exit()
 
 item_ids = [70093, 24517]
 
