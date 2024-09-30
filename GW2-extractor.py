17b38b60c2b1743517f2b19efd4e6e0772ec9fde
Jansen Smith <jansen@bstarengineering.com>
2024-09-30 13:41:42 -0400

    isolating prototype wallet code

--- GW2-extractor.py
+++ GW2-extractor.py
@@ -10,26 +10,18 @@ result = []
 verbosity = True  # Set to False if you don't want the "Adding element"
 
 for aaa in api_keys:
-    gg = GuildWars2Client(api_key=aaa)
-    
-    jj = gg.account.get()['name']
-    
-    cc = gg.characters.get()
-    for vv in cc:
-        ss = gg.charactersinventory.get(vv)
-        
-        for ooo in item_ids:
-            qq = [d['count'] for bag in ss['bags'] if bag for d in bag.get('inventory',[]) if isinstance(d, dict) and d['id'] == ooo]
-            
-            ww = sum(qq)
-            
-            nn = gg.items.get(id=ooo)['name']
-            
-            if verbosity:
-                print("Adding element:", [jj, vv, ww, nn, ooo])
-            result.append([jj, vv, ww, nn, ooo])
+	gg = GuildWars2Client(api_key=aaa)
 
-df = pd.DataFrame(result, columns=['Account Name', 'Character Name', 'Amount', 'Item Name', 'Item ID'])
+	jj = gg.account.get()['name']
+
+	for currency in gg.currencies.get():
+		ccc = gg.currencies.get(id=currency)
+		[ccc['name'], ccc['description']]
+		if verbosity:
+		    print("Adding element:", [jj, vv, ww, nn])
+		result.append([jj, vv, ww, nn])
+
+df = pd.DataFrame(result, columns=['Account Name', 'Character Name', 'Amount', 'Item Name'])
 
 filename = 'GW2_data_output.csv' 
 df.to_csv(filename, index=False)
