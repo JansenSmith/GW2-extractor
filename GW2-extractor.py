c44ed6ec4e5a2f2f99b9ca2ecd258d5b0f026eb7
Jansen Smith <jansen@bstarengineering.com>
2024-09-30 14:20:51 -0400

    added version which returns currencies on account

--- GW2-extractor.py
+++ GW2-extractor.py
@@ -1,6 +1,12 @@
 import pandas as pd
 from gw2api import GuildWars2Client
 
+def get_value(wallet, id):
+    for item in wallet:
+        if item['id'] == id:
+            return item['value']
+    return 0
+
 with open('api_keys.txt', 'r') as opened_api_file:
     api_keys = [line.strip() for line in opened_api_file.readlines()]
 
@@ -13,15 +19,20 @@ for aaa in api_keys:
 	gg = GuildWars2Client(api_key=aaa)
 
 	jj = gg.account.get()['name']
-
+	
+	vv = jj + '.wallet'
+	wallet = gg.accountwallet.get()
 	for currency in gg.currencies.get():
 		ccc = gg.currencies.get(id=currency)
-		[ccc['name'], ccc['description']]
-		if verbosity:
-		    print("Adding element:", [jj, vv, ww, nn])
-		result.append([jj, vv, ww, nn])
+		ww = get_value(wallet,currency)
+		nn = ccc['name']
+		dd = ccc['description']
+		if nn:
+			if verbosity:
+			    print("Adding element:", [jj, vv, ww, nn, dd])
+			result.append([jj, vv, ww, nn, dd])
 
-df = pd.DataFrame(result, columns=['Account Name', 'Character Name', 'Amount', 'Item Name'])
+df = pd.DataFrame(result, columns=['Account Name', 'Character Name', 'Amount', 'Item Name', 'Description'])
 
 filename = 'GW2_data_output.csv' 
 df.to_csv(filename, index=False)
