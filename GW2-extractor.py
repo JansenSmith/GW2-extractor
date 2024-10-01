2f4f447985daedbf8319179cba597d997d0f58ed
Jansen Smith <jansen@bstarengineering.com>
2024-10-01 15:08:40 -0400

    moving flags to front

--- GW2-extractor.py
+++ GW2-extractor.py
@@ -1,7 +1,11 @@
 import pandas as pd
 from gw2api import GuildWars2Client
 
+verbosity = True  # Set to False if you don't want the "Adding element"
 get_materials = False
+
+item_ids = 70093
+#item_ids = [70093, 24517]
 
 # takes in a results list, account name, nominal character name and a list
 # returns the results list with the contained items added
@@ -27,10 +31,7 @@ def get_value(wallet, id):
 with open('api_keys.txt', 'r') as opened_api_file:
     api_keys = [line.strip() for line in opened_api_file.readlines()]
 
-item_ids = [70093, 24517]
-
 result = []
-verbosity = True  # Set to False if you don't want the "Adding element"
 
 for aaa in api_keys:
 	gg = GuildWars2Client(api_key=aaa)
