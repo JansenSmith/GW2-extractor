6a58e0825d10a860b7b5d14361816823ac957feb
Jansen Smith <jansen@bstarengineering.com>
2024-10-01 14:59:01 -0400

    adding from main for merge

--- GW2-extractor.py
+++ GW2-extractor.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from gw2api import GuildWars2Client
 
+get_materials = False
+
 # takes in a results list, account name, nominal character name and a list
 # returns the results list with the contained items added
 def search_list(result, jj, vv, ll):
@@ -10,10 +12,10 @@ def search_list(result, jj, vv, ll):
 			ww = item['count']
 			ii = gg.items.get(id=oo)
 			nn = ii['name']
-			##dd = ii['description']
+			dd = ii.get('description', '')
 			if verbosity:
-			    print("Adding element:", [jj, vv, ww, nn, oo])
-			result.append([jj, vv, ww, nn, oo])
+			    print("Adding element:", [jj, vv, ww, nn, oo, dd])
+			result.append([jj, vv, ww, nn, oo, dd])
 	return result
 
 def get_value(wallet, id):
@@ -39,17 +41,45 @@ for aaa in api_keys:
 	inv = gg.accountinventory.get()
 	result = search_list(result, jj, vv, inv)
 	
-	vv = jj + '.materials'
-	inv = gg.accountmaterials.get()
-	result = search_list(result, jj, vv, inv)
+	if get_materials:
+		vv = jj + '.materials'
+		inv = gg.accountmaterials.get()
+		result = search_list(result, jj, vv, inv)
 	
 	vv = jj + '.bank'
 	inv = gg.accountbank.get()
 	result = search_list(result, jj, vv, inv)
 	
+	vv = jj + '.wallet'
+	wallet = gg.accountwallet.get()
+	for currency in gg.currencies.get():
+		ccc = gg.currencies.get(id=currency)
+		ww = get_value(wallet,currency)
+		nn = ccc['name']
+		dd = ccc.get('description', '')
+		oo = currency
+		if nn:
+			if verbosity:
+				print("Adding element:", [jj, vv, ww, nn, oo, dd])
+			result.append([jj, vv, ww, nn, oo, dd])
+			
+	cc = gg.characters.get()
+	for vv in cc:
+		ss = gg.charactersinventory.get(vv)
 
-df = pd.DataFrame(result, columns=['Account Name', 'Character Name', 'Amount', 'Item Name', "Item ID"])
+		for ooo in item_ids:
+			qq = [d['count'] for bag in ss['bags'] if bag for d in bag.get('inventory',[]) if isinstance(d, dict) and d['id'] == ooo]
+			ww = sum(qq)
+			ii = gg.items.get(id=ooo)
+			nn = ii['name']
+			dd = ii.get('description', '')
+			if verbosity:
+				print("Adding element:", [jj, vv, ww, nn, ooo, dd])
+			result.append([jj, vv, ww, nn, ooo, dd])
+	
 
-filename = 'GW2_account_output.csv' 
+df = pd.DataFrame(result, columns=['Account Name', 'Character Name', 'Amount', 'Item Name', 'Item ID', 'Description'])
+
+filename = 'GW2_data_output.csv' 
 df.to_csv(filename, index=False)
 print("Saved to:", filename)
